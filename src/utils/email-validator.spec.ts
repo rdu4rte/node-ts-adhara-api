2e9c264452f042f949c615c8bb14869eda7c8ac2
Raul Duarte <raul@grafftecnologia.com.br>
2021-04-18 09:45:54 -0300

    test: ensure EmailValidator return false if validator returns false

---
+++ src/utils/email-validator.spec.ts
@@ -0,0 +1,21 @@
+import validator from 'validator'
+import { EmailValidatorAdapter } from './email-validator'
+
+jest.mock('validator', () => ({
+  isEmail(): boolean {
+    return true
+  }
+}))
+
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter()
+}
+
+describe('EmailValidator adapter', () => {
+  test('should return false if validator returns false', () => {
+    const sut = makeSut()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+    const isValid = sut.isValid('invalid_email@mail.com')
+    expect(isValid).toBe(false)
+  })
+})
